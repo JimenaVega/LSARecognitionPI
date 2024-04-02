8a002ee4078283705fb55638904b38b7b0b58f1e
AlejoSev <alejoseverini@gmail.com>
2024-04-02 16:44:38 -0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -3,8 +3,8 @@ import time
 
 cam = cv2.VideoCapture(0)
 
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
 cam.set(cv2.CAP_PROP_FPS, 60)
 
 img_counter = 0
