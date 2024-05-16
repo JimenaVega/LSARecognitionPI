c6cf51647b0f795b039fa585dd5ce09ee00b6ad0
AlejoSev <alejoseverini@gmail.com>
2024-05-16 12:29:38 -0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -7,6 +7,10 @@ import mediapipe as mp
 import matplotlib.pyplot as plt
 import pandas as pd
 from itertools import product
+from scipy.spatial import distance
+
+HAND_FLATTEN_POINTS = 42
+POSE_FLATTEN_POINTS = 46
 
 
 signs_codes = {
@@ -224,7 +228,78 @@ def plot_lsa64_metadata(dataset_version='raw'):
     plt.show()
 
 
-def create_dataset(dataset_version='raw', sequences=50, frames=60):
+def add_dataset_padding(dataset_version='raw', sequences=50):
+
+    # Define the actions (signs) that will be recorded and stored in the dataset
+    PATH = os.path.join(f'data_{dataset_version}_zero_padded')
+
+    actions = list(signs_codes.values())
+
+    # Create directories for each action, sequence, and frame in the dataset
+    for action, sequence in product(actions, range(sequences)):
+        try:
+            os.makedirs(os.path.join(PATH, action, str(sequence), 'hands'))
+            os.makedirs(os.path.join(PATH, action, str(sequence), 'pose'))
+        except:
+            pass
+
+    signs_dirs = os.listdir(f'data_{dataset_version}')
+
+    longest_frames = 0
+
+    for sign_dir in signs_dirs:
+        sequence_dirs = os.listdir(f'data_{dataset_version}/{sign_dir}')
+
+        for sequence_dir in sequence_dirs:
+            frames = os.listdir(f'data_{dataset_version}/{sign_dir}/{sequence_dir}/hands')
+
+            if len(frames) > longest_frames:
+                longest_frames = len(frames)
+    
+    for sign_dir in signs_dirs:
+        sequence_dirs = os.listdir(f'data_{dataset_version}/{sign_dir}')
+
+        for sequence_dir in sequence_dirs:
+            hands_frames = os.listdir(f'data_{dataset_version}/{sign_dir}/{sequence_dir}/hands')
+            hands_padded_frames = []
+
+            pose_frames = os.listdir(f'data_{dataset_version}/{sign_dir}/{sequence_dir}/pose')
+            pose_padded_frames = []
+
+            total_padding = longest_frames - len(hands_frames)
+
+            if total_padding % 2 == 0:
+                lower_padding = int(total_padding / 2)
+                upper_padding = lower_padding
+            else:
+                lower_padding = int(total_padding / 2)
+                upper_padding = lower_padding + 1
+
+            for i in range(lower_padding):
+                hands_padded_frames.append(np.zeros(HAND_FLATTEN_POINTS))
+                pose_padded_frames.append(np.zeros(HAND_FLATTEN_POINTS))
+
+            for frame in hands_frames:
+                hands_padded_frames.append(np.load(f'data_{dataset_version}/{sign_dir}/{sequence_dir}/hands/{frame}'))
+            
+            for frame in pose_frames:
+                pose_padded_frames.append(np.load(f'data_{dataset_version}/{sign_dir}/{sequence_dir}/pose/{frame}'))
+
+            for i in range(upper_padding):
+                hands_padded_frames.append(np.zeros(HAND_FLATTEN_POINTS))
+                pose_padded_frames.append(np.zeros(HAND_FLATTEN_POINTS))
+
+            for i in range(len(hands_padded_frames)):
+                frame_path = os.path.join(PATH, sign_dir, str(sequence_dir), 'hands', str(i))
+                np.save(frame_path, hands_padded_frames[i])
+
+                frame_path = os.path.join(PATH, sign_dir, str(sequence_dir), 'pose', str(i))
+                np.save(frame_path, pose_padded_frames[i])
+
+            print(f'Padded for {sign_dir} {sequence_dir}/50 completed.')
+            
+
+def create_dataset(dataset_version='raw', sequences=50):
 
     # Define the actions (signs) that will be recorded and stored in the dataset
     PATH = os.path.join(f'data_{dataset_version}')
@@ -245,8 +320,6 @@ def create_dataset(dataset_version='raw', sequences=50, frames=60):
 
     with mp.solutions.holistic.Holistic(min_detection_confidence=0.75, min_tracking_confidence=0.75) as holistic:
         
-        stop_condition = 0
-
         for file_name in files:
             clip = cv2.VideoCapture(f'{folder_path}\{file_name}')
 
@@ -273,17 +346,28 @@ def create_dataset(dataset_version='raw', sequences=50, frames=60):
                 results = holistic.process(frame)
                 frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
+                # We take the shoulders, and nose points
+                # The nose point is used for translation, and the shoulders for normalization
+                left_shoulder = results.pose_landmarks.landmark[11]
+                right_shoulder = results.pose_landmarks.landmark[12]
+                shoulders_distance = distance.euclidean((left_shoulder.x, left_shoulder.y, left_shoulder.z), (right_shoulder.x, right_shoulder.y, right_shoulder.z))
+
+                nose = results.pose_landmarks.landmark[0]
+
                 # Extract the keypoints for the left hand if present, otherwise set to zeros
-                lh = np.array([[res.x, res.y] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(42)
+                lh = np.array([[(res.x - nose.x) / shoulders_distance, (res.y - nose.y) / shoulders_distance]
+                               for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(HAND_FLATTEN_POINTS)
 
                 # Extract the keypoints for the right hand if present, otherwise set to zeros
-                rh = np.array([[res.x, res.y] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(42)
+                rh = np.array([[(res.x - nose.x) / shoulders_distance, (res.y - nose.y) / shoulders_distance]
+                               for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(HAND_FLATTEN_POINTS)
 
                 # Concatenate the keypoints for both hands
                 hands_landmarks = np.concatenate([lh, rh])
 
                 #Extract the body keypoints if present, otherwise set to zeros
-                pose_landmarks = np.array([[res.x, res.y] for res in results.pose_landmarks.landmark[0:23]]).flatten() if results.pose_landmarks else np.zeros(46)
+                pose_landmarks = np.array([[(res.x - nose.x) / shoulders_distance, (res.y - nose.y) / shoulders_distance]
+                                           for res in results.pose_landmarks.landmark[0:23]]).flatten() if results.pose_landmarks else np.zeros(POSE_FLATTEN_POINTS)
 
                 frame_path = os.path.join(PATH, sign, str(sequence), 'hands', str(i))
                 np.save(frame_path, hands_landmarks)
@@ -294,11 +378,24 @@ def create_dataset(dataset_version='raw', sequences=50, frames=60):
                 # print(f'keypoints: {keypoints}')
 
             print(f'sign:{sign} ({sign_code}/64)    secuence:{sequence+1}/50 ({clips_frames} frames)')
+    
+    add_dataset_padding(dataset_version=dataset_version)
 
-            stop_condition += 1
 
-            if stop_condition >= 100:
-                break
+def plot_padding(sign, sequence):
+    sequence_path = os.path.join(f'data_cut/{sign}/{sequence}')
+    padded_sequence_path = os.path.join(f'data_cut_zero_padded/{sign}/{sequence}')
+
+    hands_landmarks = []
+    pose_landmarks = []
+
+    hands_padded_landmarks = []
+    pose_padded_landmarks = []
+
+    for frame in os.listdir(f'{sequence_path}/hands/'):
+        pass
+
+    x_axis_len = None
 
 
 dataset_version = 'cut'
@@ -306,11 +403,15 @@ dataset_version = 'cut'
 # get_lsa64_metadata(dataset_version=dataset_version)
 # plot_sign_metadata(dataset_version=dataset_version, sign='Map')
 # plot_lsa64_metadata(dataset_version=dataset_version)
-start = datetime.datetime.now()
-print(f'[{start}] Start creating dataset')
 
-create_dataset(dataset_version=dataset_version)
+# start = datetime.datetime.now()
+# print(f'[{start}] Start creating dataset')
 
-end = datetime.datetime.now()
-print(f'[{end}] Finish creating dataset')
-print(f'Elapsed time: {end - start}')
+# create_dataset(dataset_version=dataset_version)
+
+# end = datetime.datetime.now()
+# print(f'[{end}] Finish creating dataset')
+# print(f'Elapsed time: {end - start}')
+
+# add_dataset_padding(dataset_version=dataset_version)
+plot_padding(sign='Accept', sequence=0)
