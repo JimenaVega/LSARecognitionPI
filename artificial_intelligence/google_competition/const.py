515a85ebf95a2a9f6908831c9a4647bb88a25791
JimenaVega <jimevega28@gmail.com>
2024-05-31 11:16:43 -0300

    Comments in code and refactor by pycharm

--- artificial_intelligence/google_competition/const.py
+++ artificial_intelligence/google_competition/const.py
@@ -1,32 +1,33 @@
 import glob
 import numpy as np
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-DATAPATH= '/home/alejo/repos/LSARecognitionPI/'
+DATAPATH = os.getenv('DATAPATH')
 
 ROWS_PER_FRAME = 543
 MAX_LEN = 384
 CROP_LEN = MAX_LEN
-NUM_CLASSES = 64 # 250
+NUM_CLASSES = 64  # 250
 PAD = -100.
 
-NOSE=[
-    1,2,98,327
-]
+NOSE = [1, 2, 98, 327]
 LNOSE = [98]
 RNOSE = [327]
-LIP = [ 0,
-    61, 185, 40, 39, 37, 267, 269, 270, 409,
-    291, 146, 91, 181, 84, 17, 314, 405, 321, 375,
-    78, 191, 80, 81, 82, 13, 312, 311, 310, 415,
-    95, 88, 178, 87, 14, 317, 402, 318, 324, 308,
-]
-LLIP = [84,181,91,146,61,185,40,39,37,87,178,88,95,78,191,80,81,82]
-RLIP = [314,405,321,375,291,409,270,269,267,317,402,318,324,308,415,310,311,312]
+LIP = [0,
+       61, 185, 40, 39, 37, 267, 269, 270, 409,
+       291, 146, 91, 181, 84, 17, 314, 405, 321, 375,
+       78, 191, 80, 81, 82, 13, 312, 311, 310, 415,
+       95, 88, 178, 87, 14, 317, 402, 318, 324, 308,
+       ]
+LLIP = [84, 181, 91, 146, 61, 185, 40, 39, 37, 87, 178, 88, 95, 78, 191, 80, 81, 82]
+RLIP = [314, 405, 321, 375, 291, 409, 270, 269, 267, 317, 402, 318, 324, 308, 415, 310, 311, 312]
 
 POSE = [500, 502, 504, 501, 503, 505, 512, 513]
-LPOSE = [513,505,503,501]
-RPOSE = [512,504,502,500]
+LPOSE = [513, 505, 503, 501]
+RPOSE = [512, 504, 502, 500]
 
 REYE = [
     33, 7, 163, 144, 145, 153, 154, 155, 133,
@@ -40,9 +41,9 @@ LEYE = [
 LHAND = np.arange(468, 489).tolist()
 RHAND = np.arange(522, 543).tolist()
 
-POINT_LANDMARKS = LIP + LHAND + RHAND + NOSE + REYE + LEYE #+POSE
+POINT_LANDMARKS = LIP + LHAND + RHAND + NOSE + REYE + LEYE  # +POSE
 
 NUM_LANDMARKS = len(POINT_LANDMARKS)
-CHANNELS = 6*NUM_LANDMARKS
+CHANNELS = 6 * NUM_LANDMARKS
 
 TRAIN_FILENAMES = glob.glob(DATAPATH + '/tfrecords/' + '*.tfrecords')
